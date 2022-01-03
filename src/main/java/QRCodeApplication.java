ddf1f829bae51e55a02407d5e6bd5b2228ba21b6
NoCodeNolife-cloud <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2022-01-03 14:15:41 +0800

    Initial commit

---
+++ src/main/java/QRCodeApplication.java
@@ -0,0 +1,17 @@
+import com.NoCodeNoLife.QRCodeUtil;
+
+public class QRCodeApplication {
+    public static void main(String[] args) throws Exception {
+        // 存放在二维码中的内容
+        // 二维码中的内容可以是文字，可以是链接等
+        String text = "http://www.baidu.com";
+        // 生成的二维码的路径及名称
+        String destPath = "./code" + System.currentTimeMillis() + ".jpg";
+        //生成二维码
+        QRCodeUtil.encode(text, null, destPath, true);
+        // 解析二维码
+        String str = QRCodeUtil.decode(destPath);
+        // 打印出解析出的内容
+        System.out.println(str);
+    }
+}
